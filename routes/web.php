444ca2874abc5c7aa3f5714ff3d97a3003ba2aee
Yuriko <utanotantei@gmail.com>
2022-01-26 06:30:39 +0000

    Laravel10

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,12 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix'=>'admin'],function(){
-    Route::get('news/create','Admin\NewsController@add');   
+    Route::get('news2/create','Admin\NewsController@add');   
 });
 
 /*Route::get('XXX','Admin\AAAController@bbb');*/  //課題用
 
 Route::group(['prefix'=>'admin'],function(){
-    Route::get('profile/create','Admin\ProfileController@add');
-    Route::get('profile/edit','Admin\ProfileController@edit');
+    Route::get('profile2/create','Admin\ProfileController@add');
+    Route::get('profile2/edit','Admin\ProfileController@edit');
 });
